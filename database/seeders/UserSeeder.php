9bba6ef911dcc0030d0797aab040fb41e6f2c261
Maycon Medeiros <maycon.medeiros@devsquad.com>
2021-12-13 18:52:56 -0300

    add seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Lauthz\Facades\Enforcer;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $administrator = User::factory()->create(['email' => 'team@devsquad.com']);
+        Enforcer::addRoleForUser($administrator->id, 'administrator');
+
+        $editor = User::factory()->create(['email' => 'editor@devsquad.com', 'current_team_id' => $administrator->current_team_id]);
+        Enforcer::addRoleForUser($editor->id, 'editor');
+        Enforcer::addPermissionForUser($editor->id, 'section', 'read');
+
+        User::factory()->create(['email' => 'user@devsquad.com']);
+    }
+}
