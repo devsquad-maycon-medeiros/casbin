eb1d84f8436a7366e6b872de4503702851120edf
Maycon Medeiros <maycon.medeiros@devsquad.com>
2021-12-13 18:54:49 -0300

    check if a user has a permission to access route

--- app/Http/Livewire/Articles/Index.php
+++ app/Http/Livewire/Articles/Index.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\Articles;
 
+use Illuminate\Support\Facades\Auth;
+use Lauthz\Facades\Enforcer;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.articles.index');
+        if (Enforcer::enforce((string)Auth::id(), "article", "read")) {
+            return view('livewire.articles.index');
+        }
+
+        return abort(403);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,17 @@
 <?php
 
+use App\Http\Livewire\Sections;
+use App\Http\Livewire\Articles;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::view('/dashboard','dashboard')->name('dashboard');
+
+    Route::middleware(['enforcer:section,read'])->get('sections', Sections\Index::class);
+
+    Route::get('articles', Articles\Index::class);
+});
