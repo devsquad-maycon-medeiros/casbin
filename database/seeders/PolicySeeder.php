6b95401da43658fb6bf5e9bd8362b0e93b51b0a6
Maycon Medeiros <maycon.medeiros@devsquad.com>
2021-12-13 18:56:53 -0300

    remove duplicated code

--- database/seeders/PolicySeeder.php
+++ database/seeders/PolicySeeder.php
@@ -19,25 +19,10 @@ class PolicySeeder extends Seeder
         Enforcer::addPolicy('administrator', 'section', 'update');
         Enforcer::addPolicy('administrator', 'section', 'delete');
 
-        Enforcer::addPolicy('administrator', 'section', 'create');
-        Enforcer::addPolicy('administrator', 'section', 'read');
-        Enforcer::addPolicy('administrator', 'section', 'update');
-        Enforcer::addPolicy('administrator', 'section', 'delete');
-
         Enforcer::addPolicy('administrator', 'article', 'create');
         Enforcer::addPolicy('administrator', 'article', 'read');
         Enforcer::addPolicy('administrator', 'article', 'update');
         Enforcer::addPolicy('administrator', 'article', 'delete');
-
-        Enforcer::addPolicy('administrator', 'article', 'create');
-        Enforcer::addPolicy('administrator', 'article', 'read');
-        Enforcer::addPolicy('administrator', 'article', 'update');
-        Enforcer::addPolicy('administrator', 'article', 'delete');
-
-        Enforcer::addPolicy('editor', 'article', 'create');
-        Enforcer::addPolicy('editor', 'article', 'read');
-        Enforcer::addPolicy('editor', 'article', 'update');
-        Enforcer::addPolicy('editor', 'article', 'delete');
 
         Enforcer::addPolicy('editor', 'article', 'create');
         Enforcer::addPolicy('editor', 'article', 'read');
